8e7b846a84b7435a375905234ed6943dba068f2c
kamrul-hasan01 <k17h02@gmail.com>
2023-07-13 23:59:50 +0600

    redux and private  routes add

--- src/page/home.tsx
+++ src/page/home.tsx
@@ -1,4 +1,8 @@
+import { useAppSelector } from "../redux/hooks";
+
 const Home = () => {
+  const { user } = useAppSelector((state) => state.user);
+  console.log("user :", user);
   return <div>this is a home page</div>;
 };
 
---
+++ src/routes/Privateroute.tsx
@@ -0,0 +1,20 @@
+// import { useAppSelector } from "@/redux/hook";
+import { ReactNode } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAppSelector } from "../redux/hooks";
+// import { useAppSelector } from "../redux/hooks";
+
+interface IProps {
+  children: ReactNode;
+}
+
+export default function PrivateRoute({ children }: IProps) {
+  const { user } = useAppSelector((state) => state.user);
+  console.log("user :", user);
+  const { pathname } = useLocation();
+  if (!user.email) {
+    return <Navigate to="/login" state={{ path: pathname }} />;
+  }
+
+  return children;
+}
