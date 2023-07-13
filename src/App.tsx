7bf91d1d64ea21e1f49d254072f61b9c76bd7522
kamrul-hasan01 <k17h02@gmail.com>
2023-07-13 22:57:34 +0600

    tailwind setup done

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="bg-[red]">Hello</p>
+    </>
+  );
+}
+
+export default App;
